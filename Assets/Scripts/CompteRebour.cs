49acb7ccf015f6f202325aa53fcebc44b95273e1
Olivier-St-Amand79 <blackburnerik061@gmail.com>
2025-02-07 11:59:57 -0500

    Continuation

--- Assets/Scripts/CompteRebour.cs
+++ Assets/Scripts/CompteRebour.cs
@@ -5,19 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class CompteRebour : MonoBehaviour
 {
+    [SerializeField] private float _activation = 0f;
     [SerializeReference] private GameObject _bille;
-
     [SerializeReference] private GameObject _terminus;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(_activation = 1f)
+        {
+
+        }
     }
 
+    void OnCollisionEnter(Collision collision){
+        print(collision.gameObject.name + " : " + collision.gameObject.tag);
+        if(collision.gameObject.CompareTag("Terminus")){
+            _activation = 1f;
+        }
+    }
 }
